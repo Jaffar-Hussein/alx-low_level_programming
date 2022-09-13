3bd0b8c1945ab401037d1359f64a4186dc25235f
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-13 10:38:01 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,18 @@
  */
 void jack_bauer(void)
 {
-	for (int i = 0; i < 3; i++)
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (int k = 0; k < 6; k++)
+			for (k = 0; k < 6; k++)
 			{
-				for (int l = 0; l < 10; l++)
+				for (l = 0; l < 10; l++)
 				{
 					if (((i + j)  > 5))
 					{
