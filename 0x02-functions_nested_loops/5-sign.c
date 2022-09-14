2ac387e1165e300602ea9c103734a2ece6a0ce5e
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-14 12:41:25 -0700

    add sign to a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -11,17 +12,17 @@ int print_sign(int n)
 {
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 }
