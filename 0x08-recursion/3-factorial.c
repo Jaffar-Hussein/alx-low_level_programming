3cad3e454b0f13ea53800051f10001eb7f4433e6
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-28 05:37:03 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * factorial - Returns factorial
+ *
+ * @n: number entered
+ *
+ * Return: Factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
