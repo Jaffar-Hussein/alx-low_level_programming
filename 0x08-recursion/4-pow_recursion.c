6b232e641c4cb2ed857be92441a5bff499c3e868
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-28 06:45:27 -0700

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - power
+ *
+ * @x:the number
+ * @y:Factor to be raised by
+ *
+ * Return: always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	y--;
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y);
+}
