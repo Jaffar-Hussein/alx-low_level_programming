501e52693389de9fde0a7689f225cd68d3966cb6
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-28 06:16:18 -0700

    Primw

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * s_prime_number - checks if its prime
+ *
+ * @n:number to be checked
+ *
+ * Return: 1 if prime 0 otherwise
+ */
+int s_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+
+	}
+	else if (n == 2)
+		return (1);
+	}
+}
