a79c13c2b07139a2cc4264f0e3660bdb275d117d
Jaffar-Hussein <jaffhussein@icloud.com>
2022-09-28 04:40:13 -0700

    Printing string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursions - prints a string followed by a new line
+ *
+ *  Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(s + 1);
+	}
+}
